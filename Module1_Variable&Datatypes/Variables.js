310c8a47f0ac65e40c42aa9f5b53d7e3d42b8039
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-07-07 22:53:11 +0530

    01-Variable & Datatypes
    Add Module1_Variable&Datatypes/Variables.js demonstrating variable declarations (let, const, var) and console output.

---
+++ Module1_Variable&Datatypes/Variables.js
@@ -0,0 +1,14 @@
+let name="Amrita";
+let age=21;
+const country="India";
+var college="Cutm";
+
+console.log("Name : ", name);
+console.log("age : ", age);
+console.log("country : ", country);
+console.log("college: ", college);
+
+name="Ananya";
+age=22;
+console.log("updated Name:" , name);
+console.log("updated Age:", age);

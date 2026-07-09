2e470efaab9d0f248a6643b7454da6bc368b32e7
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-07-09 21:55:48 +0530

    Add Module3: Loops and String examples
    Add educational module demonstrating JavaScript loop constructs (for, while, do-while) and common string methods with practical examples.

---
+++ Module3_loops_&_String/strings.js
@@ -0,0 +1,11 @@
+let name = "Amrita";
+
+console.log(name);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.charAt(2));
+console.log(name.indexOf("r"));
+console.log(name.includes("rit"));
+console.log(name.slice(0, 4));
+console.log(name.replace("Amrita", "Ananya"));

46a159132027d4cf9b1256575b199258de0da340
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-07-10 22:58:22 +0530

    Add template literals example
    Add TemplateLiterals.js demonstrating string interpolation with template literals using variable and expression substitution.

---
+++ Module3_loops_&_String/TemplateLiterals.js
@@ -0,0 +1,9 @@
+let name = "Amrita";
+let age = 21;
+
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+let a = 10;
+let b = 20;
+
+console.log(`The sum of ${a} and ${b} is ${a + b}`);

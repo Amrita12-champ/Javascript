3eb142f325738897c8145206077bd59d9ec9a1aa
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-07-25 22:54:41 +0530

    Add DOM manipulation script for attributes
    Add JavaScript file demonstrating DOM element selection, attribute operations, and inline styling. Includes commented examples of getAttribute/setAttribute methods and active code that modifies div styling and text content.

---
+++ Module7_DOM2/attributes/script.js
@@ -0,0 +1,17 @@
+//let div=document.querySelector("div");
+//console.log(div);
+// let id =div.getAttribute("id");
+// console.log(id);
+// let name =div.getAttribute("name");
+// console.log(name);
+// let pa=document.querySelector("p");
+// //console.log(pa.getAttribute("class"));
+// console.log(pa.setAttribute("class", "newclass"));
+// console.log(pa.getAttribute("class"));
+let div=document.querySelector("div");
+div.style.fontSize="20px";
+div.innerText="hello !!"
+
+
+
+

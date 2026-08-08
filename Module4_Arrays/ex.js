1ece2bcf35a65dff238b556a20ad75923bdc1b3a
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-08-08 23:29:37 +0530

    Add array discount example (Module4_Arrays/ex.js)
    Introduce Module4_Arrays/ex.js: a small script that applies a 10% discount to each value in the sample array [12, 14, 15] and logs the final prices.

---
+++ Module4_Arrays/ex.js
@@ -0,0 +1,8 @@
+let Amrita=[12, 14, 15];
+for(let val=0; val<Amrita.length; val++){
+    let original=Amrita[val];
+    let offer=original/10;
+    let final=original-offer;
+    console.log("final price are : "+ final);
+}
+console.log()
